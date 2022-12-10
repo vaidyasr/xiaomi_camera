bd572b84104f9e5e1c2e0fd2f11d1603b51408be
root <root@raspberrypi>
2022-12-10 11:43:23 +0530

    new repository

---
+++ const.py
@@ -0,0 +1,13 @@
+"""Constants for xiaomi camera integration."""
+
+DOMAIN = "xiaomi_camera"
+
+DEFAULT_BRAND = "fang-hacks"
+DEFAULT_BRAND_R = "fang_hacks"
+
+DEFAULT_HOST = ""
+DEFAULT_PORT = 21
+DEFAULT_PATH = "/media/mmcblk0p1/record"
+DEFAULT_USERNAME = "root"
+DEFAULT_PASSWORD = ""
+DEFAULT_FFMPEG_ARGUMENTS = "-vf scale=800:450"
